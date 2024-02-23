ba77dabe1f5e6221cdfff9ceb182e7a340e5ed13
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-23 17:11:44 +0530

    Update __main__.py

--- HyperGames/__main__.py
+++ HyperGames/__main__.py
@@ -1,1 +1,29 @@
+from HyperGames import *
+from HyperGames.Database.games import *
+from pyrogram import Client, filters
+import os
+import logging
+import pyrogram
 
+# LOGGING
+logging.basicConfig(
+    format="[Hyper-Games] %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
+
+PWD = f"{os.getcwd()}/" # GETTING CURRENT PATH
+
+# START COMMAND
+@Sophia.on_message(filters.command("start", prefixes=HANDLER), filters.private)
+async def Start(_, message):
+    AVAILABLE_USERS = await GET_AVAILABLE_USERS() # GETTING AVAILABLE USERS IN DATABASE
+    USER_ID = message.from_user.id # GETTING MESSAGE FROM USER ID
+    if USER_ID in AVAILABLE_USERS: # CHECKING USER IN DATABASE
+        await message.reply_text()
+    else:
+        await message.reply_text()
+
+if __name__ == "__main__":
+    bot.start() # STARTING CLIENT
+    pyrogram.idle()
