04b96987208345c74428a21599223fef407dfbb1
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-02-23 16:58:51 +0530

    Update __init__.py

--- HyperGames/__init__.py
+++ HyperGames/__init__.py
@@ -1,1 +1,21 @@
-# WAIT
+import os
+import sys
+from pyrogram import Client
+from pyrogram import Client
+from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
+HANDLER = ["~",".","!","/","$"]
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI")
+VERSION = 0.001
+
+# CLIENT OF HYPERGAMES
+bot = Client("Hyper-Games", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="HyperGames/plugins"))
+
+# DATABASE
+
+MONGO_DB = MongoClient(MONGO_DB_URI) # Special Thanks To KoraXD For Giving This Codes!!
+GAME_DATABASE = AsyncIOMotorClient(MONGO_DB_URI)["HYPER_GAMES"]
