f257c99bf9fc7cf562cc26444aef4f005b11a17c
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-03 14:30:13 +0530

    Create delete_account.py

---
+++ HyperGames/plugins/delete_account.py
@@ -0,0 +1,20 @@
+from HyperGames import HANDLER
+from HyperGames.__main__ import bot
+from pyrogram import filters
+import asyncio
+import os
+from HyperGames.Database.games import *
+from pyrogram import enums
+import random
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+CONFIRM_BUTTON = InlineKeyboardMarkup([
+    [InlineKeyboardButton("Confirm", callback_data="DELETE_AC")]
+])
+
+@Sophia.on_message(filters.command(["delete_account", "deleteaccount"], prefixes=HANDLER))
+async def delete_account(_, message):
+    await message.reply(
+        text="Do you really want to delete account",
+        reply_markup=CONFIRM_BUTTON
+    )
