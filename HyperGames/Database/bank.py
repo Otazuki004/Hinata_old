00f3bc1c8dcc6b3723c203bf584e85048cc6352e
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-04 23:31:36 +0530

    Update bank.py

--- HyperGames/Database/bank.py
+++ HyperGames/Database/bank.py
@@ -1,7 +1,7 @@
 from HyperGames import GAME_DATABASE
 import asyncio
 import random
-from HyperGames.Database.games import GET_AVAILABLE_USERS
+from HyperGames.Database.games import *
 
 db = GAME_DATABASE["Games_RO"]
 AVAILABLE_BANKS = ['TB', 'SBT', 'HB', 'Y_AXIS_BANK', 'CB']
