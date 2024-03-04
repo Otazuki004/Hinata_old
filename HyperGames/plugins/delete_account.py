26af61071641eaba7282a2e31eaed16e8db35223
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-04 08:52:24 +0530

    Update delete_account.py

--- HyperGames/plugins/delete_account.py
+++ HyperGames/plugins/delete_account.py
@@ -12,6 +12,8 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 @bot.on_message(filters.command(["delete_account", "deleteaccount"], prefixes=HANDLER))
 async def delete_account(_, message):
     user_id = message.from_user.id
+    if user_id not in await GET_AVAILABLE_USERS():
+        return await message.reply("You don't have a account to delete.")
     await message.reply(
         text="Do you really want to delete account",
         reply_markup=InlineKeyboardMarkup([
