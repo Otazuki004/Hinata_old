8ad9f1c86e8169b78b6869d4d8886dc2cc92ecc3
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-10 20:04:25 +0530

    Update __init__.py

--- HyperGames/__init__.py
+++ HyperGames/__init__.py
@@ -8,6 +8,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
 HANDLER = ["~",".","!","/","$"]
+DEVELOPER_ID = set(int(x) for x in os.environ.get("DEVELOPER_ID", "").split())
 MONGO_DB_URI = os.environ.get("MONGO_DB_URI")
 VERSION = 0.001
 
@@ -17,6 +18,14 @@ if len(BOT_TOKEN) > 150:
 else:
     bot = Client("Hyper-Games", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="HyperGames/plugins"))
 
+# FUNCTION
+async def developer(_, update, _):
+    message=update
+    if message.from_user.id in DEVELOPER_ID:
+        return True
+    else:
+        return False
+        
 # DATABASE
 MONGO_DB = MongoClient(MONGO_DB_URI) # Special Thanks To KoraXD For Giving This Codes!!
 GAME_DATABASE = AsyncIOMotorClient(MONGO_DB_URI)["HYPER_GAMES"]
