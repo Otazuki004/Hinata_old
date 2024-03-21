62b234948ab2a6b1b448639d3f458b7c51ec48af
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-21 18:57:46 +0530

    Update run.py

--- HyperGames/plugins/run.py
+++ HyperGames/plugins/run.py
@@ -1,11 +1,11 @@
 import os, sys, io, traceback
 from datetime import datetime
-from HyperGames.Database.games import *
+from Hinata.Database.main import *
 from contextlib import redirect_stdout
 from subprocess import getoutput as run
 from pyrogram.enums import ChatAction
-from HyperGames import bot as app
-from HyperGames import *
+from Hinata import bot as app
+from Hinata import *
 from pyrogram import filters
 
 prefix = [".","!","?","*","$","#","/"]
