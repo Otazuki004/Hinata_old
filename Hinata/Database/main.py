3f0d8e9c81af0ae93c9889f1ce26b97c7946588f
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-24 15:13:10 +0530

    Update main.py

--- Hinata/Database/main.py
+++ Hinata/Database/main.py
@@ -336,7 +336,7 @@ async def GET_LEVEL(user_id: int):
 
 async def BET_BLOCKED(user_id: int, REMOVE=False):
     if REMOVE == False:
-        await db.update_one({"_id": 1}, {"$inc": {f"{user_id}_BET_BLOCK": 1}})
+        await db.update_one({"_id": 1}, {"$inc": {f"{user_id}_BET_BLOCK": 1}}, upsert=True)
         user_data = await db.find_one({"_id": 1})
         COUNT = user_data.get(f"{user_id}_BET_BLOCK")
         if COUNT >= 5:
@@ -347,7 +347,9 @@ async def BET_BLOCKED(user_id: int, REMOVE=False):
         if user_data:
             COUNT = user_data.get(f"{user_id}_BET_BLOCK")
             if REMOVE == True:
-                await db.update_one({"_id": 1}, {"$inc": {f"{user_id}_BET_BLOCK": {-COUNT}}})
+                COUNT = f"-{COUNT}"
+                COUNT = int(COUNT)
+                await db.update_one({"_id": 1}, {"$inc": {f"{user_id}_BET_BLOCK": COUNT}}, upsert=True)
                 return "SUCCESS"
             
 async def SPAM_CONTROL(user_id: int, GET=False):
