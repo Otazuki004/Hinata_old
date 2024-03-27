7ebffad0a640a7334c0e0d67e4858cb856ba5d2f
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-03-27 22:05:05 +0530

    Update __init__.py

--- Hinata/__init__.py
+++ Hinata/__init__.py
@@ -35,7 +35,7 @@ MONGO_DB = MongoClient(MONGO_DB_URI) # Special Thanks To KoraXD For Giving This 
 GAME_DATABASE = AsyncIOMotorClient(MONGO_DB_URI)["HYPER_GAMES"]
 
 # LIST COMMANDS IN BOT
-get_commands = bot.get_bot_commands()
+from Hinata.__main__ import get_commands
 
 commands = []
 
